8383f527f0d989da6749f7dc90c6c1b595da23ec
McKenna Jo Beardsley <mckennajo@McKennas-MacBook-Pro.local>
2018-02-11 20:06:43 -0800

    FIRST UPDATE

---
+++ liri.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+var fs = require("fs");
+
+var request = require("request");
+
+var keys = require("./keys.js");
+
+var Twitter = require('twitter');
+
+// var Spotify = require("node-spotify-api");
+
+console.log(keys);
+
+
+
+// var liriArgument = process.argv[2];
+// var userINPUT = process.argv[3];
+// var spotify = new Spotify(keys.spotify);
+// var client = new Twitter(keys.twitter);
